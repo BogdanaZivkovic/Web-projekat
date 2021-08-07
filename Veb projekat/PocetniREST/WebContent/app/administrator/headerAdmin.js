5697d25e81adb651d00ab43c74b13b6cb53a05fb
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2021-08-07 13:26:11 +0200

    header za administratora

---
+++ Veb projekat/PocetniREST/WebContent/app/administrator/headerAdmin.js
@@ -0,0 +1,13 @@
+Vue.component("header-admin", {
+	template:`
+	<div>
+		<nav>
+			<ul>
+				<li><router-link to="/" exact> Profile </router-link></li>
+				<li> <router-link to="/usersadmin" exact> Users </router-link> </li>
+			</ul>
+		</nav>
+	</div>
+	`
+	
+});
