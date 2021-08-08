cc764598950e24ca0011b6337d49753fdfabd065
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2021-08-08 22:12:18 +0200

    osnovno alfanumericko sortiranje

--- Veb projekat/PocetniREST/WebContent/app/allRestaurants.js
+++ Veb projekat/PocetniREST/WebContent/app/allRestaurants.js
@@ -11,6 +11,8 @@ Vue.component("all-restaurants-app", {
 	},
 	template: `
 	<div>
+		<button @click="sortAlphanumeric"> Sort alphanumeric </button>
+		<button @click="sortAlphanumericReverse"> Sort alphanumeric reverse </button>
 		<div v-if="searchVisible">
 			<form method='post'>
 				<input type="text" v-model="searchFields.name" placeholder="Naziv"></input>
@@ -42,7 +44,39 @@ Vue.component("all-restaurants-app", {
 			if(!restaurant.name.match(this.searchFields.name))
 				return false;
 			return true;
-		}
+		},
+		sortAlphanumeric: function () {
+			this.restaurants.sort(this.alphaNumCriterium);
+		},
+		sortAlphanumericReverse: function () {
+			this.restaurants.sort(this.reverseAlphaNumCriterium);
+		},
+		alphaNumCriterium: function (a,b) {
+      		var reA = /[^a-zA-Z]/g;
+      		var reN = /[^0-9]/g;
+      		var aA = a.name.replace(reA, "");
+      		var bA = b.name.replace(reA, "");
+      		if(aA === bA) {
+          		var aN = parseInt(a.name.replace(reN, ""), 10);
+          		var bN = parseInt(b.name.replace(reN, ""), 10);
+          		return aN === bN ? 0 : aN > bN ? 1 : -1;
+      		} else {
+          		return aA > bA ? 1 : -1;
+      		}
+    	},
+		reverseAlphaNumCriterium: function (a,b) {
+      		var reA = /[^a-zA-Z]/g;
+      		var reN = /[^0-9]/g;
+      		var aA = a.name.replace(reA, "");
+      		var bA = b.name.replace(reA, "");
+      		if(aA === bA) {
+          		var aN = parseInt(a.name.replace(reN, ""), 10);
+          		var bN = parseInt(b.name.replace(reN, ""), 10);
+          		return aN === bN ? 0 : aN > bN ? -1 : 1;
+      		} else {
+          		return aA > bA ? -1 : 1;
+      		}
+    	}
 	},
 	mounted () {
 		axios
