3244f6de3b9ce1cc47e5591134bc44aa1c6996e9
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2021-08-09 18:31:46 +0200

    svi restorani menadzer

--- Veb projekat/PocetniREST/WebContent/app/manager/appManager.js
+++ Veb projekat/PocetniREST/WebContent/app/manager/appManager.js
@@ -1,11 +1,13 @@
 const ProfileManager = {template:'<edit-user></edit-user>'}
 const ViewRestaurantManager = {template:'<view-restaurant-manager></view-restaurant-manager>'}
+const RestaurantsManager = {template:'<all-restaurants-app></all-restaurants-app>'}
 
 const router = new VueRouter({
 	mode: 'hash',
 	  routes: [
 		{ path: '/', component: ProfileManager},
-		{path: '/restaurantmanager', component: ViewRestaurantManager}
+		{path: '/restaurantmanager', component: ViewRestaurantManager},
+		{path: '/allrestaurantsmanager', component: RestaurantsManager}
 	]
 });
 
--- Veb projekat/PocetniREST/WebContent/app/manager/headerManager.js
+++ Veb projekat/PocetniREST/WebContent/app/manager/headerManager.js
@@ -5,6 +5,7 @@ Vue.component("header-manager", {
 			<ul>
 				<li><router-link to="/" exact> Profile </router-link></li>
 				<li> <router-link to="/restaurantmanager" exact> My restaurant </router-link> </li>
+				<li> <router-link to="/allrestaurantsmanager" exact> Restaurants </router-link> </li>
 			</ul>
 		</nav>
 	</div>
