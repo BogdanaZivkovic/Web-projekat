4548d4167c6b0f0acdd9d67729583547bccd1d15
BogdanaZivkovic <73473560+BogdanaZivkovic@users.noreply.github.com>
2021-08-26 17:25:17 +0200

    header css

--- Veb projekat/PocetniREST/WebContent/app/header.js
+++ Veb projekat/PocetniREST/WebContent/app/header.js
@@ -1,14 +1,36 @@
 Vue.component("header-app", {
 	template:`
-	<div>
-		<nav>
-			<ul>
-				<li> <router-link to="/register" exact> Register </router-link> </li>
-				<li><router-link to="/login" exact> Login </router-link></li>
-				<li><router-link to="/restaurants" exact> Restaurants </router-link></li>
+	<div class="container">
+		<nav class="navbar navbar-expand-md navbar-light">
+		<a href="#" class="navbar-brand"> 
+			<img src="images/logo.png" alt="FDLogo" width="100" height="100" class="d-inline-block align-top"/> 
+			<span class="hidden"> FD </span> 
+		</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#toggleMobileMenu"
+			aria-controls="toggleMobileMenu"
+			aria-expanded="false"
+			aria-lable="Toggle navigation"
+			>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="toggleMobileMenu">
+			<ul class="navbar-nav text-center">
+				<li>
+				<router-link class="nav-link" to="/register" exact> Register </router-link> 
+				</li>
+				<li>
+				<router-link class="nav-link" to="/login" exact> Login </router-link>
+				</li>
+				<li>
+				<router-link class="nav-link" to="/restaurants" exact> Restaurants </router-link>
+				</li>
 			</ul>
+		</div>
 		</nav>
 	</div>
 	`
-	
 });
