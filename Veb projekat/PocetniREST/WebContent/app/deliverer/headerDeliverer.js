86f3a481090b9356fe1ae4f45f023975dae69ad0
BogdanaZivkovic <73473560+BogdanaZivkovic@users.noreply.github.com>
2021-08-29 19:00:18 +0200

    deliverer header

--- Veb projekat/PocetniREST/WebContent/app/deliverer/headerDeliverer.js
+++ Veb projekat/PocetniREST/WebContent/app/deliverer/headerDeliverer.js
@@ -1,13 +1,38 @@
 Vue.component("header-deliverer", {
 	template:`
-	<div>
-		<nav>
-			<ul>
-				<li><router-link to="/" exact> Profile </router-link></li>
-				<li><router-link to="/orders" exact> Orders </router-link></li>
-				<li><router-link to="/myorders" exact> Deliveries </router-link></li>
-				<li><button @click="logout" > Log out </button></li>
+	<div class="container">
+		<nav class="navbar navbar-expand-md navbar-light">
+		<a href="#" v-on:click="$router.push('/')" class="navbar-brand"> 
+			<img src="images/logo.png" alt="FDLogo" width="90" height="60" class="d-inline-block align-top"/> 
+			<span class="hidden"> FD </span> 
+		</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#toggleMobileMenu"
+			aria-controls="toggleMobileMenu"
+			aria-expanded="false"
+			aria-lable="Toggle navigation"
+			>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="toggleMobileMenu">
+			<ul class="navbar-nav text-center">
+				<li>
+					<router-link class="nav-link" to="/" exact> Profile </router-link>
+				</li>
+				<li>
+					<router-link class="nav-link" to="/orders" exact> Orders </router-link> 
+				</li>
+				<li>
+					<router-link class="nav-link" to="/myorders" exact> Deliveries </router-link>
+				</li>
+				<li>
+					<a href="#" class="nav-link" v-on:click="logout"> Log out </a>
+				</li>
 			</ul>
+		</div>
 		</nav>
 	</div>
 	`,
