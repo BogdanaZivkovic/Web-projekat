d5b27b11049b62a9565f6ea3c9b00355b994a921
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2021-08-30 21:20:42 +0200

    ne radi al sam se smorio

--- Veb projekat/PocetniREST/WebContent/app/administrator/restaurantsAdmin.js
+++ Veb projekat/PocetniREST/WebContent/app/administrator/restaurantsAdmin.js
@@ -13,10 +13,31 @@ Vue.component("restaurants-admin", {
 				status:''
 			},
 			restaurants: [],
-			images: []
+			images: [],
+			newRestaurant: {
+				name: "",
+				type: "",
+				status: "",
+				managerUsername: "",
+				city:"",
+				street:"",
+				number:"",
+				zipCode:"",
+				logoPath: "",
+			},
+			newUser: {
+				userName: "",
+				password: "",
+				name: "",
+				surname: "",
+				gender: "",
+				dateOfBirth: ""
+			},
+			managers: []
 		}
 	},
 	template: `
+	<div>
 	<div class="container-fluid bg">
 	<div class="row justify-content-center" >
 			<div class="col-lg-6 col-md-6 col-sm-12 search-area ">
@@ -98,7 +119,7 @@ Vue.component("restaurants-admin", {
 		<div class="row justify-content-center mb-2">
 			<div class="col-lg-8 col-md-10 col-sm-12">
 				<div class="row float-end me-2">
-					<button class="btn btn-primary" @click="$router.push('/restaurantadmin')"> + New restaurant </button>
+					<button class="btn btn-primary" @click="clearModal" data-bs-toggle="modal" data-bs-target="#exampleModal"> + New restaurant </button>
 				</div>
 			</div>
 		</div>
@@ -139,8 +160,190 @@ Vue.component("restaurants-admin", {
 			</div>
 		</div>
 	</div>
+	
+	
+	
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-dialog-centered">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">New restaurant</h5>
+	        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+	      </div>
+		<form @submit="createRestaurant(newRestaurant, newUser)" method='post'>
+	      <div class="modal-body">
+	       				  <div class="mb-3">
+						    <label for="nameInput" class="form-label">Restaurant name</label>
+						    <input v-model="newRestaurant.name" id="nameInput" type="text" class="form-control" placeholder="Restaurant name" required>
+						  </div>
+						  <div class="mb-3">
+					   		<label for="typeInput" class="form-label"> Type </label>
+					    	<select v-model="newRestaurant.type" id="typeInput" class="form-select">
+								<option disabled value="">Please select one</option>
+							    <option>Chinese</option>
+							    <option>Italian</option>
+								<option>Fast food</option>
+								<option>BBQ</option>
+								<option>Mexican</option>
+								<option>Gyros</option>
+							</select>
+					  	  </div>
+						  <div class="mb-3">
+						    <label for="imageInput" class="form-label">Image</label>
+						    <input id="imageInput" type="file" class="form-control" onchange="encodeImageFileAsURLForChanging(this)" required>
+							<img hidden id="imgForChangeID"  src="" alt="Image of restaurant" width="11" height="11">
+						  </div>
+						  <div class="mb-3">
+						    <label for="cityInput" class="form-label"> City </label>
+						    <input v-model="newRestaurant.city" id="cityInput" type="text" class="form-control" placeholder="City" required>
+						  </div>
+						  <div class="mb-3">
+						    <label for="streetInput" class="form-label"> Street </label>
+						    <input v-model="newRestaurant.street" id="streetInput" type="text" class="form-control" placeholder="Street" required>
+						  </div>
+						  <div class="mb-3">
+						    <label for="numberInput" class="form-label"> Number </label>
+						    <input v-model="newRestaurant.number" id="numberInput" type="text" class="form-control" placeholder="Number" required>
+						  </div>
+						  <div class="mb-3">
+						    <label for="zipCodeInput" class="form-label"> Zip code </label>
+						    <input v-model="newRestaurant.zipCode" id="zipCodeInput" type="text" class="form-control" placeholder="ZIP code" required>
+						  </div>
+						  <div v-if="managers.length!=0" class="mb-3">
+						    <label for="managerInput" class="form-label"> Manager </label>
+						    <select v-model="newRestaurant.managerUsername" id="managerInput" class="form-select">
+								<option v-for="manager in managers" v-bind:value=manager.userName>
+                       				{{ manager.name + " " + manager.surname}}
+                   				</option>
+							</select>
+						  </div>
+						  <div v-if="managers.length==0">
+							<p> There are no available managers. Register a new manager for this restaurant. </p>
+							<div class="mb-3">
+						   		<label for="managerNameInput" class="form-label">Name</label>
+						    	<input v-model="newUser.name" id="managerNameInput" type="text" class="form-control" placeholder="Name" required>
+							</div>
+						  	<div class="mb-3">
+						    	<label for="surnameInput" class="form-label"> Surname </label>
+						    	<input v-model="newUser.surname" id="surnameInput" type="text" class="form-control" placeholder="Surname" required>
+						  	</div>
+						  	<div class="mb-3">
+						    	<label for="usernameInput" class="form-label"> Username </label>
+						    	<input v-model="newUser.userName" id="usernameInput" type="text" class="form-control" placeholder="Username" required>
+						  	</div>
+							<div class="mb-3">
+						    	<label for="passwordInput" class="form-label"> Password </label>
+						    	<input v-model="newUser.password" id="passwordInput" type="text" class="form-control" placeholder="Password" required>
+						  	</div>
+						  	<div class="mb-3">
+					   			<label for="genderInput" class="form-label"> Gender </label>
+					    		<select v-model="newUser.gender" id="genderInput" class="form-select">
+									<option disabled value="">Please select one</option>
+							    	<option>Male</option>
+							    	<option>Female</option>
+								</select>
+					  	 	</div>
+						  	<div class="mb-3">
+						    	<label for="dateOfBirthInput" class="form-label"> Date of birth </label>
+								<input type="date" v-model="newUser.dateOfBirth" min="1950-01-01" max="2020-01-01" id="dateOfBirthInput" class="form-control">
+						  	</div>
+						</div>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+	        <button type="submit" class="btn btn-primary"> Confirm </button>
+	      </div>
+		</form>
+	    </div>
+	  </div>
+	</div>
+
+	
+	</div>
 	`,
     methods: {
+		clearModal: function () {
+			axios
+          	.get('rest/users/getAvailableManagers')
+          	.then(response => (this.managers = response.data))
+			
+			this.newRestaurant = {
+				name: "",
+				type: "",
+				status: "",
+				managerUsername: "",
+				city:"",
+				street:"",
+				number:"",
+				zipCode:"",
+				logoPath: "",
+			};
+			
+			this.newUser = {
+				userName: "",
+				password: "",
+				name: "",
+				surname: "",
+				gender: "",
+				dateOfBirth: ""
+			};
+		},
+		createRestaurant : function (newRestaurant, newUser) {
+			
+			this.newRestaurant.logoPath = document.getElementById("imgForChangeID").src;
+			
+			if(this.managers.length != 0) {
+			axios
+			.post('rest/restaurants/create', {
+				"name":''+newRestaurant.name, 
+				"type":''+newRestaurant.type, 
+				"status":''+"Open",
+				"managerUsername":''+newRestaurant.managerUsername,
+				"street":''+newRestaurant.street,
+				"number":''+newRestaurant.number,
+				"city":''+newRestaurant.city,
+				"zipCode":''+newRestaurant.zipCode,
+				"logoPath":''+newRestaurant.logoPath
+				
+			})
+			.then(response => {
+				$('#exampleModal').modal('hide');
+				//this.init();
+			})
+			}
+			
+			else {
+				axios.all([
+			axios
+			.post('rest/restaurants/create', {
+				"name":''+newRestaurant.name, 
+				"type":''+newRestaurant.type, 
+				"status":''+"Open",
+				"managerUsername":''+newUser.userName,
+				"street":''+newRestaurant.street,
+				"number":''+newRestaurant.number,
+				"city":''+newRestaurant.city,
+				"zipCode":''+newRestaurant.zipCode,
+				"logoPath":''+newRestaurant.logoPath
+				}),
+			axios
+			.post('rest/users/registration', {
+				"userName":''+newUser.userName, 
+				"password":''+newUser.password, 
+				"name":''+newUser.name, 
+				"surname":''+newUser.surname, 
+				"gender":''+newUser.gender, 
+				"dateOfBirth":''+newUser.dateOfBirth, 
+				"role":''+"MANAGER"
+				})
+			])
+			.then(response => {
+				$('#exampleModal').modal('hide');
+				//this.init();
+			})
+		}
+		},
 		matchesSearch: function (restaurant) {
 			if(!restaurant.name.toLowerCase().match(this.searchFields.name.toLowerCase()))
 				return false;
@@ -226,9 +429,9 @@ Vue.component("restaurants-admin", {
 			.then(response => {
 				this.restaurants = response.data
 				this.restaurants.sort((a, b) => (a.status > b.status) ? -1 : 1);
-				axios.get('rest/images/getImages')
+			})
+			axios.get('rest/images/getImages')
 				.then(response => (this.images = response.data));
-			})	
 		},
 		deleteRestaurant : function (restaurant) {
 			if (confirm('Are you sure?') == true) {
